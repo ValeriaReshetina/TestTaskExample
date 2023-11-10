b031c3aec2e5599c3737e8be542b35e0cb82850e
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-11-10 13:06:35 +0300

    added Owner to project

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,34 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:remote.properties"
+})
+
+public interface WebConfig extends Config {
+
+    @Key("browser")
+    @DefaultValue("CHROME")
+    String getBrowser();
+
+    @Key("browserVersion")
+    @DefaultValue("99.0")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("baseUrl")
+    @DefaultValue("https://store.steampowered.com/")
+    String getBaseUrl();
+
+    @Key("remoteUrl")
+    @DefaultValue("")
+    String getRemoteUrl();
+
+    @Key("remoteAuth")
+    @DefaultValue("")
+    String getRemoteAuth();
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,28 +2,43 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.closeWindow;
 import static helpers.AttachHelper.*;
 
 public class TestBase {
+
+    protected static final WebConfig config = ConfigFactory.create(WebConfig.class, System.getProperties());
+
     @BeforeAll
     static void beforeAll() {
-        Configuration.remote = System.getProperty("remoteUrl");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
-        Configuration.browserVersion = System.getProperty("browserVersion", "100");
+        Configuration.baseUrl = config.getBaseUrl();
+        Configuration.browser = config.getBrowser();
+        Configuration.browserVersion = config.getBrowserVersion();
+        Configuration.browserSize = config.getBrowserSize();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
+        String selenoidHome = config.getRemoteUrl();
+        String selenoidCredentials = config.getRemoteAuth();
+        if (!(selenoidHome.isEmpty() || selenoidCredentials.isEmpty())) {
+            Configuration.remote = "https://" + selenoidCredentials + "@" + selenoidHome + "/wd/hub";
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("selenoid:options", Map.of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+
+        }
     }
 
     @BeforeEach
@@ -33,10 +48,11 @@ public class TestBase {
 
     @AfterEach
     void addAttachments() {
-        //screenshotAs("Last screenshot");
+        screenshotAs("Last screenshot");
         pageSource();
         browserConsoleLogs();
         addVideo();
+
         closeWebDriver();
         closeWindow();
     }
